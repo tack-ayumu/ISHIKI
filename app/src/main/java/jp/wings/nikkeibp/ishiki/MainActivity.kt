d1d89d765918d41644c3a15b59c3d11a09a82ce3
tack-ayumu <gaze696@gmail.com>
2020-02-21 21:22:15 +0900

    add image and text,fix layout

--- app/src/main/java/jp/wings/nikkeibp/ishiki/MainActivity.kt
+++ app/src/main/java/jp/wings/nikkeibp/ishiki/MainActivity.kt
@@ -15,10 +15,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //ボタンなどの画面部品を取得する
-
         //textViewを背景ごと消しておくための処置
         textView.visibility = View.INVISIBLE
+        //初期メッセージの表示
+        textView_Atstart_reset.setText(R.string.msg_atStart)
+        textView_Description.visibility = View.VISIBLE
+
+        //メッセージ誤表示防止のためresetボタンの無効化
+        stop.isEnabled = false
+        reset.isEnabled = false
+
+
 
         val typedArray =
             resources.getStringArray(R.array.list_random_msg)
@@ -40,11 +47,29 @@ class MainActivity : AppCompatActivity() {
 
         start.setOnClickListener{
             handler.post(runnable)
+            //startボタン二度押しによる誤作動防止
+            start.isEnabled = false
+            stop.isEnabled = true
+
+            //初期メッセージ,リセット時メッセージの非表示
+            textView_Atstart_reset.setText("")
+            textView_Description.visibility = View.INVISIBLE
+            //進行中、リセット時のメッセージを表示
+            textView_Inprogress.setText(R.string.msg_inprogress)
+
         }
+
 
         stop.setOnClickListener{
             handler.removeCallbacks(runnable)
-            textView2.setText(R.string.msg_question)
+            //進行中のメッセージを消す
+            textView_Inprogress.setText("")
+
+            //resetボタンの有効化
+            reset.isEnabled = true
+
+            //stopボタン二度押し防止
+            stop.isEnabled = false
 
             when(timeValue){
                 in 1..10 -> {
@@ -54,6 +79,7 @@ class MainActivity : AppCompatActivity() {
                     textView.visibility = View.VISIBLE
                     //textViewの中にランダムでメッセージを表示させる
                     textView.setText(values);
+                    textView2.setText(R.string.msg_question)
                 }
 
                 in 11..30 -> {
@@ -61,11 +87,10 @@ class MainActivity : AppCompatActivity() {
                     val values2 = typedArray2.get(rand)
                     //textViewを背景とセットで出し・・・
                     textView.visibility = View.VISIBLE
-                    //textViewの中にランダムでメッセージを表示させる
+                    //textViewの中にランダムでメッセージを表示
                     textView.setText(values2);
+                    textView2.visibility = View.VISIBLE
                 }
-
-
 
             }
 
@@ -79,12 +104,23 @@ class MainActivity : AppCompatActivity() {
             timeToText()?.let{
                 timeText.text = it
 
+                //表示されたメッセージの初期化
                 textView.setText("")
-                textView.setVisibility(View.INVISIBLE)
+                textView.visibility = View.INVISIBLE
                 textView2.setText("")
-
+                //リセット時のメッセージの表示
+                textView_Atstart_reset.setText(R.string.msg_atReset)
             }
+
+            //スタート・ストップボタンの二度押しによる誤作動防止を解除
+            start.isEnabled = true
+            stop.isEnabled = false
+            reset.isEnabled = false
+
         }
+
+
+
     }
 
     fun timeToText(time:Int = 0):String?{
